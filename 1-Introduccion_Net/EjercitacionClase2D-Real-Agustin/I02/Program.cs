7e13e5b7b7edb1b88cc6751521114664849d30c8
AgustinReal <agustinreal26@gmail.com>
2022-08-22 22:04:17 -0300

    Ejercicio I02 realizado

---
+++ 1-Introduccion_Net/EjercitacionClase2D-Real-Agustin/I02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace I02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            bool convertido;
+            double respuestaUno;
+            double respuestaDos;
+
+            Console.WriteLine("Ingrese un numero: ");
+            convertido = int.TryParse(Console.ReadLine(), out numero);
+
+            if (numero > 0 && convertido)
+            {
+                respuestaUno = Math.Pow(numero, 2);
+                respuestaDos = Math.Pow(numero, 3);
+
+                Console.WriteLine($"El numero ingresado es: {numero}");
+                Console.WriteLine($"El cuadrado del numero ingresado es: {respuestaUno}");
+                Console.WriteLine($"El cubo del numero ingresado es: {respuestaDos}");
+            }
+            else
+            {
+                Console.WriteLine("ERROR. ¡Reingresar número!");
+            }
+        }
+    }
+}
